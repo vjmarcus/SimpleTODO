7ac73eec84ea4e76ee51500ad01526075510e427
vjmarcus <vjmarcus1@gmail.com>
2019-04-23 11:57:34 +0300

    Added notifyDataSetChanged

--- app/src/main/java/com/vjmarcus/simpletodo/MainActivity.java
+++ app/src/main/java/com/vjmarcus/simpletodo/MainActivity.java
@@ -2,10 +2,13 @@ package com.vjmarcus.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,11 +19,13 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     Button mButton;
     EditText mEditText;
+    private static final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditText = (EditText)findViewById(R.id.editText_new_item);
 
         listView = (ListView)findViewById(R.id.listview);
         items = new ArrayList<>();
@@ -30,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 40; i++) {
             items.add(i + " item");
         }
+       // setupListViewListener();
+
+        mButton = (Button)findViewById(R.id.button_add_item);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Button mButton PUSH");
+                items.add(mEditText.getText().toString());
+                mEditText.setText("");
+                itemAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+
     }
+
 }
