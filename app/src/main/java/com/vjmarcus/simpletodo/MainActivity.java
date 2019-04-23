15f270f730131c8a1bd0049f731af817b41c4da1
vjmarcus <vjmarcus1@gmail.com>
2019-04-23 11:33:08 +0300

    Added descendantFocusability - focus for EditText

--- app/src/main/java/com/vjmarcus/simpletodo/MainActivity.java
+++ app/src/main/java/com/vjmarcus/simpletodo/MainActivity.java
@@ -2,12 +2,33 @@ package com.vjmarcus.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String > items;
+    ArrayAdapter<String> itemAdapter;
+    ListView listView;
+    Button mButton;
+    EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listview);
+        items = new ArrayList<>();
+        itemAdapter = new ArrayAdapter<>(MainActivity.this,
+                R.layout.support_simple_spinner_dropdown_item, items);
+        listView.setAdapter(itemAdapter);
+        for (int i = 0; i < 40; i++) {
+            items.add(i + " item");
+        }
     }
 }
