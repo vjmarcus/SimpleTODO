bca7453c61f4a0d015be4ee72dbf5c5a13d9e80f
vjmarcus <vjmarcus1@gmail.com>
2019-04-23 13:39:58 +0300

    Updated OnItemLongClickListener

--- app/src/main/java/com/vjmarcus/simpletodo/MainActivity.java
+++ app/src/main/java/com/vjmarcus/simpletodo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.vjmarcus.simpletodo;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String > items;
+    ArrayList<String> items;
     ArrayAdapter<String> itemAdapter;
     ListView listView;
     Button mButton;
@@ -26,9 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEditText = (EditText)findViewById(R.id.editText_new_item);
+        mEditText = (EditText) findViewById(R.id.editText_new_item);
 
-        listView = (ListView)findViewById(R.id.listview);
+        listView = (ListView) findViewById(R.id.listview);
         items = new ArrayList<>();
         itemAdapter = new ArrayAdapter<>(MainActivity.this,
                 R.layout.support_simple_spinner_dropdown_item, items);
@@ -36,19 +38,37 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 40; i++) {
             items.add(i + " item");
         }
-       // setupListViewListener();
+        // setupListViewListener();
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                items.remove(position);
-                itemAdapter.notifyDataSetChanged();
-                Toast.makeText(MainActivity.this, "Deleated", Toast.LENGTH_SHORT).show();
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(R.string.dialog_message);
+                // Add the buttons
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        items.remove(position);
+                        itemAdapter.notifyDataSetChanged();
+                        Toast.makeText(MainActivity.this, "Deleated", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
                 return true;
             }
         });
 
-        mButton = (Button)findViewById(R.id.button_add_item);
+        mButton = (Button) findViewById(R.id.button_add_item);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 items.add(mEditText.getText().toString());
                 mEditText.setText("");
                 itemAdapter.notifyDataSetChanged();
+
 
             }
         });
